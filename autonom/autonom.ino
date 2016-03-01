80acd9904377d7d0290af58ddd9cbb108c4d9d78
martinlunde <martlun@stud.ntnu.no>
2016-03-01 15:21:41 +0100

    sensor implementation

--- autonom/autonom.ino
+++ autonom/autonom.ino
@@ -47,70 +47,79 @@ void loop() {
     motors.setSpeeds(kjorepaa,kjorepaa);
     sensors.read(sensor_values);
     
-    
-    
-    //Check if border has been detected by any of the two sensors at each side
-    if ((sensor_values[0] < QTR_THRESHOLD) || (sensor_values[5] < QTR_THRESHOLD)) { // Venstre er 0
-      motors.setSpeeds(0,0);//Stop,
-      if(timeSinceStart - incrementTime > 200 && !drivingBackwards){
-      // Change to opposite direction
-          if (direction){
-            direction  =LOW;
-          }else{
-             direction  =HIGH;
-          }
-      if(sensor_values[0] < QTR_THRESHOLD){
-        leftHadLight = true;
-      }
-      else{
-        rightHadLight = true;
-      }
-      motors.flipRightMotor(direction);
-      motors.flipLeftMotor(direction);
-      motors.setSpeeds(kjorepaa, kjorepaa);
-      drivingBackwards = true;
-      reversing = timeSinceStart;
+   if(!(drivingBackwards && turningRight && turningLeft)){
+      if(right_sensor_input == HIGH){
+         //sving 90 grader høyre
+     
+      }else if(left_sensor_input == HIGH){
+         //sving 90 grader venstre
+     
+      }else if(back_sensor_input == HIGH){
+         //sving 180 grader
    }
-    incrementTime = timeSinceStart;
-  }
-
-//Venter til bilen har rygga i 200 millisekund.
-  if(drivingBackwards && timeSinceStart - reversing > 200){
-    drivingBackwards = false;
-    hasDrivenBackwards = true;
-    if (direction){
-      direction  =LOW;
-    }else{
-      direction  =HIGH;
-    }
-    motors.setSpeeds(0,0);
-  }
-
-//Får bilen til å svinge etter bilen har rygga.
-  if (hasDrivenBackwards && leftHadLight){
-    motors.flipLeftMotor(direction);
-    motors.setSpeeds(kjorepaa, kjorepaa);
-    turningRight = true;
-    turnTime = timeSinceStart;
-    leftHadLight = false;
-    hasDrivenBackwards = false;
-  }if (hasDrivenBackwards && rightHadLight){
-    motors.flipRightMotor(direction);
-    motors.setSpeeds(kjorepaa, kjorepaa);
-    turningLeft = true;
-    turnTime = timeSinceStart;
-    rightHadLight = false;
-    hasDrivenBackwards = false;
-  }
-
-  //Lar bilen kjøre framover igjen etter 250 millisekund. 
-  if(timeSinceStart - turnTime > 250 && (turningRight || turningLeft)){
-    direction = LOW;
-    motors.flipLeftMotor(direction);
-    motors.flipRightMotor(direction);
-    motors.setSpeeds(kjorepaa, kjorepaa);
-    turningRight = false;
-    turningLeft = false;
-    //turnTime = timeSinceStart;
-  }
+   }else{
+       //Check if border has been detected by any of the two sensors at each side
+       if ((sensor_values[0] < QTR_THRESHOLD) || (sensor_values[5] < QTR_THRESHOLD)) { // Venstre er 0
+         motors.setSpeeds(0,0);//Stop,
+         if(timeSinceStart - incrementTime > 200 && !drivingBackwards){
+         // Change to opposite direction
+             if (direction){
+               direction  =LOW;
+             }else{
+                direction  =HIGH;
+             }
+         if(sensor_values[0] < QTR_THRESHOLD){
+           leftHadLight = true;
+         }
+         else{
+           rightHadLight = true;
+         }
+         motors.flipRightMotor(direction);
+         motors.flipLeftMotor(direction);
+         motors.setSpeeds(kjorepaa, kjorepaa);
+         drivingBackwards = true;
+         reversing = timeSinceStart;
+      }
+       incrementTime = timeSinceStart;
+      }
+      
+      //Venter til bilen har rygga i 200 millisekund.
+      if(drivingBackwards && timeSinceStart - reversing > 200){
+       drivingBackwards = false;
+       hasDrivenBackwards = true;
+       if (direction){
+         direction  =LOW;
+       }else{
+         direction  =HIGH;
+       }
+       motors.setSpeeds(0,0);
+      }
+      
+      //Får bilen til å svinge etter bilen har rygga.
+      if (hasDrivenBackwards && (leftHadLight||rightHadLight)){
+       if(leftHadLight){
+          motors.flipLeftMotor(direction);
+       }
+       if(rightHadLight){
+          motors.flipRightMotor(direction);
+       }
+       motors.setSpeeds(kjorepaa, kjorepaa);
+       turningRight = true;
+       turnTime = timeSinceStart;
+       leftHadLight = false;
+       rightHadLight = false;
+       hasDrivenBackwards = false;
+      }
+      
+      //Lar bilen kjøre framover igjen etter 250 millisekund. 
+      if(timeSinceStart - turnTime > 250 && (turningRight || turningLeft)){
+       direction = LOW;
+       motors.flipLeftMotor(direction);
+       motors.flipRightMotor(direction);
+       motors.setSpeeds(kjorepaa, kjorepaa);
+       turningRight = false;
+       turningLeft = false;
+       //turnTime = timeSinceStart;
+      }
+   }
 }
